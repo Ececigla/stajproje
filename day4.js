3f47c19c35abb678b914c405cc1161831606f93a
Ececigla <ececiglaaksoy@gmail.com>
2024-07-09 16:08:58 +0300

    proje

---
+++ day4.js
@@ -0,0 +1,38 @@
+let result = prompt('2+5 kaç yapar?', '2+5')
+
+if(result === '7') {
+    console.log("doğru cevap verdin")
+} else {
+    console.log("hatalı cevasp verdin, cevap 7 olmalıydı")
+}
+
+
+
+switch(result){
+
+    case '7':
+    console.log("doğru cevap - switch")
+    break
+
+    case '5':
+    console.log("hatalı cevap")
+    break
+
+    default:
+        console.log("hatalı cevap, switch")
+
+
+
+}
+
+
+let number = 5
+if(number ===4) {
+
+} else if (number < 5 ){
+
+} else if (number > 5 ){
+
+} else{
+
+}
