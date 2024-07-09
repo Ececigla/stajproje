0c440dd17c4a856bde224c4cacd25e9a99bdd740
Ececigla <ececiglaaksoy@gmail.com>
2024-07-09 12:39:12 +0300

    first commit

---
+++ data-types.js
@@ -0,0 +1,25 @@
+let numOne = 3
+let numTwo = 3
+
+console.log( numOne == numTwo )
+
+
+let js = 'Javascript'
+let py = 'Python'
+console.log( Javascript == Python )
+
+let nums = [1 , 2 , 3]
+nums[0] //1
+nums[1] //2
+nums[2] //3
+
+
+let nums1 = [1 , 2, 3]
+let nums2 = [1 , 2, 3]
+console.log(nums1 == nums2) //false
+
+let nums1 = [1 , 2, 3]
+let nums2 = let nums1
+console.log(nums1 == nums2) //true
+
+
---
+++ introduction.js
@@ -0,0 +1,13 @@
+console.log(typeof "ece")
+console.log(typeof 5)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+
+let personName = "ece",
+     personAge = 20,
+     personStudent = true ,
+     personMoney = undefined
+
+const PI = 3.14  //değişmeyen değerler için 'const'  kullanırız
+
